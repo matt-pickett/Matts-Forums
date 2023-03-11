70fa8dac9389eae2e6a8babaf42f6def4f26563e
Matthew Pickett <mpickett@pdx.edu>
2023-03-11 00:46:42 -0800

    Add error checking for server

--- server/index.js
+++ server/index.js
@@ -27,6 +27,29 @@ mongoose.connect(
 
 // Set up Auth0
 const { auth, requiresAuth } = require('express-openid-connect');
+
+
+// Verify that required dependencies are installed
+try {
+  require.resolve('express-openid-connect');
+} catch (error) {
+  console.error('Please install the express-openid-connect package:', error);
+  process.exit(1);
+}
+
+// Verify that environment variables are set up correctly
+if (!process.env.ISSUER_BASE_URL || !process.env.BASE_URL || !process.env.CLIENT_ID || !process.env.SECRET) {
+  console.error('Please set up the required environment variables for authentication.');
+  process.exit(1);
+}
+
+// Verify that the app object is defined
+if (!app) {
+  console.error('Please define the app object before using the auth middleware.');
+  process.exit(1);
+}
+
+// Set up the auth middleware with the appropriate options
 app.use(
   auth({
     authRequired: false,
@@ -39,23 +62,16 @@ app.use(
   })
 );
 
+// Verify that the middleware options are set up correctly
+if (!app._router.stack.some(layer => layer.name === 'openid')) {
+  console.error('Please check the auth middleware options and try again.');
+  process.exit(1);
+}
+
 app.get('/status', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out');
 });
 
 app.get('/profile', requiresAuth(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
-});
-
-
-app.get('/', (req, res) => {
-    res.send('Home page');
-});
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.listen(PORT, function () {
-    console.log('http://localhost: ', PORT);
 });
